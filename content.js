6c7d04d4e95832482588d029b15c7ece1ddbe653
guillermocoding <gs8rj@virginia.edu>
2020-12-14 22:26:11 -0500

    refactor

--- content.js
+++ content.js
@@ -1,26 +1,28 @@
-let placed = false;
+let insert = true;
 const CAT_API_URL = "https://api.thecatapi.com/v1/images/search";
 
 browser.runtime.onMessage.addListener(async () => {
-  if (!placed) {
-    const image = document.createElement("img");
+  if (insert) {
     const response = await fetch(CAT_API_URL);
     const data = await response.json();
     const imageUrl = data[0].url;
+    const image = document.createElement("img");
 
     image.setAttribute("id", "catify-image");
     image.setAttribute("src", imageUrl);
+
     Object.assign(image.style, {
       height: "400px",
       position: "absolute",
       top: 0,
       zIndex: 2147483647
     });
+
     document.body.appendChild(image);
-    placed = true;
+    insert = false;
   } else {
     const catifyImage = document.getElementById("catify-image");
     catifyImage.remove();
-    placed = false;
+    insert = true;
   }
 });
